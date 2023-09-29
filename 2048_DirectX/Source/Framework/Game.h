cc839e726f3eb52bee83373919d3bc5c74faf008
SSK-0423 <b1900446@planet.kanazawa-it.ac.jp>
2023-09-29 20:17:11 +0900

    プリコンパイル済みヘッダー追加

--- 2048_DirectX/Source/Framework/Game.h
+++ 2048_DirectX/Source/Framework/Game.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <chrono>
 #include "Window.h"
+
 namespace Framework
 {
 	/// <summary>
---
+++ 2048_DirectX/Source/pch.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// プリコンパイル済みヘッダー
+
+#include <Windows.h>
+#include <cassert>
+
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <memory>
+#include <string>
